456be1d9a23e338c2df26674762a8de8f66aa17e
Rob <robert.bell@rxpservices.com>
2021-11-20 15:35:04 +1100

    Updates

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -19,7 +19,7 @@ class App extends Component{
     }
   }
 
-  startGame(numTiles) {
+  startGame = (numTiles) => {
     this.setState((state) => ({
       playing: true,
       previousTileIndex: null,
