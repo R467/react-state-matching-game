e68afabe40454b103d890fc27e736e1acab31fca
Rob <robert.bell@rxpservices.com>
2021-11-20 15:26:26 +1100

    Stage 2

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Board.css';
+import Tile from '../Tile';
 
 const Board = (props) => {
 
@@ -8,9 +9,13 @@ const Board = (props) => {
     gridTemplateRows: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
   }
 
+  let tiles = props.tile.map((tile) => 
+      <Tile props={...tile} />
+  );
 
   return (
     <div className='Board' style={gridConfig}>
+      {tiles}
     </div>
   )
 }
